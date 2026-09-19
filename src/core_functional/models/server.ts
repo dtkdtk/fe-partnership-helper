7827575f5fd6b26c8f0e3a92cb6886d6067a1759
dtkdtk <dtkdtk0.mail@gmail.com>
2026-09-19 21:42:05 +0300

    Patch-fix

--- src/core_functional/models/server.ts
+++ src/core_functional/models/server.ts
@@ -2,6 +2,17 @@ import { AsceticInvite, ServerData } from "#core_functional";
 import { DB_Misc, DB_ServersData, updateDatedVal } from "#corelib";
 
 
+function emptyServerData() {
+  return {
+    timestamp: Date.now(),
+    message_id: null,
+    last_name: "",
+    last_members_count: 0,
+    delegates: {},
+    partners: {},
+  };
+}
+
 export async function initServerData(
   guildId: string,
   guildName: string,
@@ -9,13 +20,10 @@ export async function initServerData(
   overrides?: Partial<ServerData>
 ): Promise<ServerData | null> {
   const data: ServerData = {
+    ...emptyServerData(),
     _id: guildId,
-    timestamp: Date.now(),
-    message_id: null,
     last_name: guildName,
     last_members_count: memberCount,
-    delegates: {},
-    partners: {},
     ...overrides,
   };
   const success = Boolean(await DB_ServersData.insertAsync(data).catch(() => false));
@@ -44,6 +52,7 @@ export async function updateServerData(
   timestamp?: number
 ): Promise<ServerData> {
   const data: ServerData = {
+    ...emptyServerData(),
     ...oldData,
     last_name: guildName,
     last_members_count: memberCount,
